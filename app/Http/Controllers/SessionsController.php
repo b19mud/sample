a9e332041e7c6ee2f89e6586227ced58702ebcc0
bigmud <lihaijianbaojian@163.com>
2018-07-14 14:28:56 +0800

    Finish user edit,update,index and destroy actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -19,10 +19,10 @@ class SessionsController extends Controller
 	{
 		$credentials = $this->validate($request,['email'=>'required|email|max:255','password'=>'required']);
 
-		if (Auth::attempt($credentials,$request->has('remember')));
+		if (Auth::attempt($credentials,$request->has('remember')))
 		{
 			session()->flash('success','welcome back!');
-			return redirect()->route('users.show',[Auth::user()]);
+			return redirect()->intended(route('users.show',[Auth::user()]));
 		}else{
 			session()->flash('danger','sorry');
 			return redirect()->back();
@@ -35,5 +35,10 @@ class SessionsController extends Controller
 		Auth::logout();
 		session()->flash('success','exit!');
 		return redirect('login');
+	}
+
+	public function __construct()
+	{
+		$this->middleware('guest',['only'=>['create']]);
 	}
 }
